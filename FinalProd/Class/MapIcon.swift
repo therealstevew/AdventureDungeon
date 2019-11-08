58223cb43769aec7b04723b8670f3853d1cc48b4
SeaManSteve <44621443+SeaManSteve@users.noreply.github.com>
2019-11-08 12:27:53 -0500

    User and MapIcon Class

---
+++ FinalProd/Class/MapIcon.swift
@@ -0,0 +1,13 @@
+//
+//  MapIcon.swift
+//  FinalProd
+//
+//  Created by Steven Winstanley on 2019-11-08.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class MapIcon: NSObject {
+
+}
