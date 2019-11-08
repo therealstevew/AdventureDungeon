e370f035b22e9749fa7d5a561f8883d01f11f3e6
Andrew Mckie <ajmckie02@gmail.com>
2019-11-08 12:31:54 -0500

    added shop viewcontorller

---
+++ FinalProd/ViewControllers/ShopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShopViewController.swift
+//  FinalProd
+//
+//  Created by Andrew Mckie on 2019-11-08.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
