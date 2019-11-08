4c511a75fb71f17380d965b15999b9fc4071992f
SeaManSteve <44621443+SeaManSteve@users.noreply.github.com>
2019-11-08 11:31:32 -0500

    Initial Commit

---
+++ FinalProd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProd
+//
+//  Created by Steven Winstanley on 2019-11-08.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FinalProdTests/FinalProdTests.swift
@@ -0,0 +1,34 @@
+//
+//  FinalProdTests.swift
+//  FinalProdTests
+//
+//  Created by Steven Winstanley on 2019-11-08.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import XCTest
+@testable import FinalProd
+
+class FinalProdTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
