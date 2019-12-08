0ca462f0aa9f800f1e04e575c904a8bc72386c8b
AlexNidhogg <alexteo10@gmail.com>
2019-12-07 20:38:48 -0500

    Added Beakthing

---
+++ FinalProd/Class/EnemyAttacks/Peck.swift
@@ -0,0 +1,13 @@
+//
+//  Peck.swift
+//  FinalProd
+//
+//  Created by Alex Teodorescu on 2019-12-07.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class Peck: Attack {
+
+}
