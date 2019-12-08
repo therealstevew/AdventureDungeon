1dd536fc82fd5c016e028dd2ce0a2445640d31ec
Chris <vingilot66@hotmail.com>
2019-12-08 15:16:22 -0500

    Fixed abilities

---
+++ FinalProd/Class/Attacks/Bolster.swift
@@ -0,0 +1,13 @@
+//
+//  Bolster.swift
+//  FinalProd
+//
+//  Created by Christopher Reynolds on 2019-12-08.
+//  Copyright © 2019 Steven Winstanley. All rights reserved.
+//
+
+import UIKit
+
+class Bolster: NSObject {
+
+}
