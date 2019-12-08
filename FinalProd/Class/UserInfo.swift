0008311c1caabfb25edca887206dbed814a71a37
AlexNidhogg <alexteo10@gmail.com>
2019-12-08 14:43:32 -0500

    added stuff to userinfo

--- FinalProd/Class/UserInfo.swift
+++ FinalProd/Class/UserInfo.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class UserInfo: NSObject {
+    
+    var Heroes : [Hero] = []
+    var Items : [Item] = []
+    
     func Saveuser(Name : String, Password : String, Hero1 : Int, Hero2 : Int, Hero3 : Int, Hero4 : Int, Weapon1 : Int, Weapon2 : Int, Weapon3 : Int, Weapon4 : Int, Armour1 : Int, Armour2 : Int, Armour3 : Int, Armour4 : Int){
         print("Saving the user data probally.")
         let Address : URL = URL(string: "http://mckieand.dev.fast.sheridanc.on.ca/finalprod/postUser.php?Name=" + Name + "&Password=" + Password + "&Hero1=" + String(Hero1) + "&Hero2=" + String(Hero2) + "&Hero3=" + String(Hero3) + "&Hero4=" + String(Hero4) + "&Weapon1=" + String(Weapon1) + "&Weapon2=" + String(Weapon2) + "&Weapon3=" + String(Weapon3) + "&Weapon4=" + String(Weapon4) + "&Armour1=" + String(Armour1) + "&Armour2=" + String(Armour2) + "&Armour3=" + String(Armour3) + "&Armour4=" + String(Armour4))!
@@ -28,6 +32,5 @@ class UserInfo: NSObject {
             }
         })
         Task.resume()
-        
     }
 }
